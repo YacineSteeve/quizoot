d39be22f4624d6c8e7b3347f4cf383e7a08b5881
merchrist.kiki <alexismerchrist.kiki@gmail.com>
2022-10-04 13:00:20 +0200

    migrate to typescript and use prettier + eslint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+    name: string;
+}>();
+</script>
+        
 <template>
     <div>
-        <h1>Hello World</h1>
+        <h1>Hello {{ name }}</h1>
     </div>
 </template>
-
-<script>
-</script>
-
-<style scoped>
-</style>
+    
+        
