1420128b050df041cfb5ac4010378706f4953580
Yacine BOUKARI <steeveboukari9@gmail.com>
2022-11-12 00:02:44 +0100

    feat(ui): make the current UI responsive

--- frontend/src/components/QuizCard.vue
+++ frontend/src/components/QuizCard.vue
@@ -14,8 +14,10 @@ const bgColor = computed(() => (quiz.status ? '#008000' : '#dfe2ec'));
 
 <template>
     <div class="quiz-preview-container">
-        <div class="quiz-status" v-on:click="quiz.status = !quiz.status">
-            <font-awesome-icon icon="fa-solid fa-check" color="#ffffff" size="l" v-if="quiz.status" />
+        <div class="quiz-status-container">
+            <div class="quiz-status" v-on:click="quiz.status = !quiz.status">
+                <font-awesome-icon icon="fa-solid fa-check" color="#ffffff" size="l" v-if="quiz.status"/>
+            </div>
         </div>
         <div class="quiz-details">
             <div class="quiz-title">
@@ -25,8 +27,10 @@ const bgColor = computed(() => (quiz.status ? '#008000' : '#dfe2ec'));
                 <p>{{ quiz.description }}</p>
             </div>
         </div>
-        <div class="quiz-details-expand-icon">
-            <font-awesome-icon icon="fa-solid fa-chevron-right" size="l" />
+        <div class="quiz-details-expand-icon-container">
+            <div class="quiz-details-expand-icon">
+                <font-awesome-icon icon="fa-solid fa-chevron-right" size="l"/>
+            </div>
         </div>
     </div>
 </template>
@@ -36,7 +40,9 @@ const bgColor = computed(() => (quiz.status ? '#008000' : '#dfe2ec'));
     display: flex;
     align-items: start;
     width: 75%;
+    min-width: 600px;
     height: 16vh;
+    min-height: 110px;
     margin-block: 3vh;
     border-radius: 10px;
     box-shadow: 0 5px 20px rgba(0, 0, 0, 0.5);
@@ -49,14 +55,21 @@ const bgColor = computed(() => (quiz.status ? '#008000' : '#dfe2ec'));
     cursor: pointer;
 }
 
-.quiz-preview-container .quiz-status,
-.quiz-details-expand-icon {
+.quiz-preview-container .quiz-status-container,
+.quiz-preview-container .quiz-details-expand-icon-container {
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 5%;
-    margin-inline: 3%;
-    margin-top: 5.5vh;
+    width: 11%;
+    height: 100%;
+}
+
+.quiz-preview-container .quiz-status,
+.quiz-preview-container .quiz-details-expand-icon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
     aspect-ratio: 1 / 1;
     transform: scale(1);
 }
@@ -69,36 +82,43 @@ const bgColor = computed(() => (quiz.status ? '#008000' : '#dfe2ec'));
 .quiz-preview-container .quiz-details {
     display: flex;
     flex-direction: column;
-    justify-content: flex-end;
+    justify-content: center;
     align-items: stretch;
     text-align: left;
     width: 78%;
     height: 100%;
 }
 
+@media only screen and (max-width: 600px) {
+    .quiz-preview-container {
+        width: 85%;
+        min-width: auto;
+        height: 14vh;
+        margin-block: 2vh;
+    }
+
+    .quiz-preview-container .quiz-status-container,
+    .quiz-preview-container .quiz-details-expand-icon-container {
+        width: 14%;
+    }
+
+    .quiz-preview-container .quiz-details {
+        width: 72%;
+    }
+}
+
 .quiz-preview-container .quiz-details>* {
-    width: 100%;
-    height: 8vh;
-}
-
-.quiz-preview-container .quiz-details .quiz-title {
     display: flex;
-    justify-content: center;
     align-items: center;
-}
-
-.quiz-preview-container .quiz-details .quiz-title h2 {
+    justify-content: center;
     width: 100%;
-    height: fit-content;
-    margin-bottom: 1vh;
-}
-
-.quiz-preview-container .quiz-details .quiz-description p {
-    margin-top: 1vh;
+    height: 35%;
 }
 
 .quiz-preview-container .quiz-details>*>* {
+    width: 100%;
     height: fit-content;
+    margin: 0;
     text-overflow: ellipsis;
     white-space: nowrap;
     overflow: hidden;
