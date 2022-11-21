3898b64abe763b73d6a95e7298f657427b41dc59
Yacine BOUKARI <steeveboukari9@gmail.com>
2022-11-21 21:58:54 +0100

    feat(api): add and setup `django-rest-framework`

--- backend/settings.py
+++ backend/settings.py
@@ -23,13 +23,11 @@ env = environ.Env(
     ),
 )
 
-
 # reading .env file
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 SECRET_KEY = env("SECRET_KEY")
 
@@ -37,9 +35,7 @@ DEBUG = not env("IN_PROD")
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
-
-# Application definition
-
+# Applications definition
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -47,6 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "backend.api.apps.ApiConfig",
 ]
 
 MIDDLEWARE = [
@@ -79,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "backend.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -93,7 +90,6 @@ DATABASES = {
         },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -113,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -124,7 +119,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
--- backend/urls.py
+++ backend/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("api/", include("backend.api.urls")),
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="/api/", permanent=True)),
 ]
