7994b044070f6fa285198166309c3ba4e77d5a43
Yacine BOUKARI <steeveboukari9@gmail.com>
2022-12-12 00:31:21 +0100

    style: format

--- frontend/src/lib/http_client.ts
+++ frontend/src/lib/http_client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: 'http://localhost:8000/api'
+    baseURL: 'http://localhost:8000/api',
     // Other configurations if needed
 });
 
