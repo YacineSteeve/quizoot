7bde40aeb716597bd3d161ad7bc8644f04fb84a2
merchrist.kiki <alexismerchrist.kiki@gmail.com>
2022-12-12 22:51:06 +0100

    Implement `ChoiceQuestion`

--- frontend/src/components/questions/ChoiceQuestion.vue
+++ frontend/src/components/questions/ChoiceQuestion.vue
@@ -1,5 +1,71 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import type { Ref } from 'vue';
+import type { Quizoot } from '@interfaces/quizoot';
+
+interface ChoiceQuestionProps {
+    spec: Quizoot.ChoiceQuestion;
+}
+
+const props = defineProps<ChoiceQuestionProps>();
+
+const isSingleChoiceQuestion = (
+    spec: Quizoot.ChoiceQuestion
+): spec is Quizoot.SingleChoiceQuestion => {
+    return 'answer_id' in spec;
+};
+
+type InputType = 'checkbox' | 'radio';
+
+const answers: Ref<Set<Quizoot.OptionId>> = ref(new Set());
+
+const inputType = computed<InputType>(() =>
+    isSingleChoiceQuestion(props.spec) ? 'radio' : 'checkbox'
+);
+
+function onSelect(event: Event) {
+    const target = event.target as HTMLInputElement;
+    if (target.checked) {
+        answers.value.add(target.id);
+    } else {
+        answers.value.delete(target.id);
+    }
+}
+</script>
+
 <template>
-    <div>Hello <code>ChoiceQuestion</code></div>
+    <div class="choices">
+        <ul>
+            <li v-for="option in spec.options">
+                <input
+                    :type="inputType"
+                    :id="option.id"
+                    name="choice"
+                    @change="onSelect"
+                />
+                <label>{{ option.display }}</label>
+            </li>
+        </ul>
+    </div>
 </template>
 
-<style></style>
+<style scoped>
+.choices {
+    width: 80%;
+    text-align: left;
+    font-size: 1.2em;
+    margin: 0;
+}
+
+.choices ul {
+    list-style: none;
+}
+
+.choices li {
+    margin-block: 20px;
+}
+
+.choices input {
+    margin-inline-end: 10px;
+}
+</style>
