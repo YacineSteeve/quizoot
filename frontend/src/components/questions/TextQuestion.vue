28d02a3d829b620d2b23d9a813c71d1ec2f4e30c
Merchrist <58826859+chrichri17@users.noreply.github.com>
2022-12-14 00:45:12 +0100

    Implement `TextQuestion` (#52)
    * Implement `TextQuestion`
    
    * rm non used type

--- frontend/src/components/questions/TextQuestion.vue
+++ frontend/src/components/questions/TextQuestion.vue
@@ -1,5 +1,39 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import type { Quizoot } from '@interfaces/quizoot';
+
+interface TextQuestionProps {
+    spec: Quizoot.TextQuestion;
+}
+
+const props = defineProps<TextQuestionProps>();
+
+const answer = ref('');
+
+const placeholder = computed(
+    () => props.spec.placeholder ?? 'Type your answer here...'
+);
+</script>
+
 <template>
-    <div>Hello <code>TextQuestion</code></div>
+    <div class="text-question">
+        <input :placeholder="placeholder" v-model="answer" />
+    </div>
 </template>
 
-<style></style>
+<style scoped>
+.text-question {
+    width: 80%;
+    text-align: center;
+}
+
+input {
+    min-height: 30px;
+    width: 90%;
+    border-radius: 5px;
+    border: 2px solid var(--palette-mobster);
+    padding: 5px;
+    margin-bottom: 50px;
+    font-size: 1.1em;
+}
+</style>
