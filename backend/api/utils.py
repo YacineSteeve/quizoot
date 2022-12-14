45c7fce6d5d0e5e370d1ed3ebb40eadb1c4737a9
merchrist.kiki <alexismerchrist.kiki@gmail.com>
2022-12-14 02:23:21 +0100

    refactor view and better typing

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,29 +1,28 @@
 import json
-from bson import json_util
-from typing import Any, List, Tuple, Type
+import bson
+from typing import Any
 
 import pymongo
+
+from pymongo.collection import Collection
 from rest_framework.response import Response
 from rest_framework.request import Request
 
-from .exceptions import CollectionFull
+from django.http.request import QueryDict
 
 
-__Collection = type(Type[pymongo.collection.Collection])
-__ApiRequest = type(Type[Request])
+DEFAULT_SORT_KEY = "id"
 
-MIN_ID_VALUE = 100_000_000_000
-MAX_ID_VALUE = 999_999_999_999
-DEFAULT_QUERY_LIST_SORT_KEY = "id"
-DEFAULT_QUERY_LIST_SORT_ORDER = pymongo.ASCENDING
-QUERY_LIST_SORT_ORDER = {
+DEFAULT_SORT_ORDER = pymongo.ASCENDING
+
+SORT_ORDER = {
     "ascending": pymongo.ASCENDING,
     "descending": pymongo.DESCENDING,
 }
 
 
 def _to_python_object(obj: Any) -> Any:
-    return json.loads(json_util.dumps(obj))
+    return json.loads(bson.json_util.dumps(obj))
 
 
 class JsonResponse(Response):
@@ -31,40 +30,16 @@ class JsonResponse(Response):
         super().__init__(_to_python_object(data), *args, **kwargs)
 
 
-def get_sort_order_from_request(
-    request: Request, collection: __Collection
-) -> List[Tuple[str, int]]:
+def get_sort_order(query_params: QueryDict, collection: Collection):
+    sort_key = query_params.get("sort_key", DEFAULT_SORT_KEY)
+    sort_key = str(sort_key).lower()
+    # TODO: Find a way to check if the sort_key in a valid key of the `collection`.
 
-    sort_key = request.query_params.get("sort_key", None)
-    sort_order = request.query_params.get("sort_order", None)
-
-    if sort_key is None:
-        sort_key = DEFAULT_QUERY_LIST_SORT_KEY
-    else:
-        sort_key = str(sort_key).lower()
-        # TODO: Find a way to check if the sort_key in a valid key of the `collection`.
-
-    if sort_order is None:
-        sort_order = DEFAULT_QUERY_LIST_SORT_ORDER
-    else:
-        sort_order = QUERY_LIST_SORT_ORDER.get(
-            str(sort_order).lower(), DEFAULT_QUERY_LIST_SORT_ORDER
-        )
+    sort_order = query_params.get("sort_order")
+    sort_order = SORT_ORDER.get(str(sort_order).lower(), DEFAULT_SORT_ORDER)
 
     return [(sort_key, sort_order)]
 
 
-def get_new_id(collection: __Collection) -> str:
-    if collection.find_one() is None:
-        return str(MIN_ID_VALUE)
-    else:
-        current_max_id = _to_python_object(
-            collection.find().sort("id", pymongo.DESCENDING).limit(1)
-        )[0]["id"]
-
-        if int(current_max_id) < MAX_ID_VALUE:
-            return str(int(current_max_id) + 1)
-        else:
-            raise CollectionFull(
-                f"The {collection.name} is full! You can't create a new object."
-            )
+def get_new_id() -> str:
+    return str(bson.ObjectId())
