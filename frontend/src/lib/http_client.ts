e13419ca5bc54d83959ebcdd66ac7690ae4ada76
Merchrist <58826859+chrichri17@users.noreply.github.com>
2022-12-15 00:58:39 +0100

    Upgrade requests lib : axios and server proxy (#53)
    * Upgrade requests lib : axios and server proxy
    
    * add env example

--- frontend/src/lib/http_client.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const client = axios.create({
-    baseURL: 'http://localhost:8000/api',
-    // Other configurations if needed
-});
-
-export default client;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,28 @@
 import { URL, fileURLToPath } from 'node:url';
+import { defineConfig, loadEnv } from 'vite';
 
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue(), vueJsx()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+    const PORT = env.VITE_QUIZOOT_SERVER_PORT ?? '8000';
+
+    return {
+        plugins: [vue(), vueJsx()],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            },
         },
-    },
+        server: {
+            proxy: {
+                '/api': {
+                    target: `http://localhost:${PORT}`,
+                    changeOrigin: true,
+                },
+            },
+        },
+    };
 });
