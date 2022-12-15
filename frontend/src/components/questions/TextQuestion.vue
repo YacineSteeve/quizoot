fdc501d24d02e9c76b2f4f83e5cc1cbb5cf1fc2c
merchrist.kiki <alexismerchrist.kiki@gmail.com>
2022-12-15 16:42:17 +0100

    Hum....

--- frontend/src/components/questions/TextQuestion.vue
+++ frontend/src/components/questions/TextQuestion.vue
@@ -1,5 +1,37 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import type { Quizoot } from '@interfaces/quizoot';
+interface TextQuestionProps {
+    spec: Quizoot.TextQuestion;
+}
+const props = defineProps<TextQuestionProps>();
+const answer = ref('');
+const placeholder = computed(
+    () => props.spec.placeholder ?? 'Type your answer here...'
+);
+</script>
+
 <template>
-    <div>Hello <code>TextQuestion</code></div>
+    <div class="text-question">
+        <input :placeholder="placeholder" v-model="answer" />
+        Owner YacineSteeve Souldn't you add the attribute minlength=1 or
+        required ? Also, don't we need here a textarea instead of input ?
+        Reply... Resolve conversation
+    </div>
 </template>
 
-<style></style>
+<style scoped>
+.text-question {
+    width: 80%;
+    text-align: center;
+}
+input {
+    min-height: 30px;
+    width: 90%;
+    border-radius: 5px;
+    border: 2px solid var(--palette-mobster);
+    padding: 5px;
+    margin-bottom: 50px;
+    font-size: 1.1em;
+}
+</style>
