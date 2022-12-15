3c90b81515c6356d77210097044ad293e88514b5
Merchrist <58826859+chrichri17@users.noreply.github.com>
2022-12-15 16:52:28 +0100

    Hum.... (#60)
    * Hum....
    
    * rm review comment
    
    * use textarea
    
    * change litening port

--- frontend/src/components/questions/TextQuestion.vue
+++ frontend/src/components/questions/TextQuestion.vue
@@ -1,5 +1,38 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import type { Quizoot } from '@interfaces/quizoot';
+interface TextQuestionProps {
+    spec: Quizoot.TextQuestion;
+}
+const props = defineProps<TextQuestionProps>();
+const answer = ref('');
+const placeholder = computed(
+    () => props.spec.placeholder ?? 'Type your answer here...'
+);
+</script>
+
 <template>
-    <div>Hello <code>TextQuestion</code></div>
+    <div class="text-question">
+        <!-- TODO: add rows in spec as optional field -->
+        <textarea
+            :placeholder="placeholder"
+            rows="8"
+            v-model="answer"
+        ></textarea>
+    </div>
 </template>
 
-<style></style>
+<style scoped>
+.text-question {
+    width: 80%;
+    text-align: center;
+}
+textarea {
+    width: 100%;
+    border-radius: 5px;
+    border: 2px solid var(--palette-mobster);
+    padding: 5px;
+    margin-bottom: 50px;
+    font-size: 1.1em;
+}
+</style>
