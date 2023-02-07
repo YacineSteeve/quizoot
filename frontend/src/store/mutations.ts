3f1e132dcd8b00f0d33b46fed9f6450952425788
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-02-07 14:14:39 +0100

    Add `Vue-x` (#65)
    * build: add `vue-x`
    
    * feat: setup the `store`
    
    * refactor: temporary change the data to fit the current schema specs.

---
+++ frontend/src/store/mutations.ts
@@ -0,0 +1,16 @@
+import type { MutationTree } from 'vuex';
+import type { State } from '@/store/state';
+import { MutationTypes } from '@/store/types';
+
+export type Mutations<S = State> = {
+    [MutationTypes.UPDATE_QUIZZES](
+        state: S,
+        quizzesList: State['quizzes']
+    ): void;
+};
+
+export const mutations: MutationTree<State> & Mutations = {
+    [MutationTypes.UPDATE_QUIZZES](state, quizzesList: State['quizzes']) {
+        state.quizzes = quizzesList;
+    },
+};
---
+++ frontend/src/store/state.ts
@@ -0,0 +1,9 @@
+import type { Quiz } from '@interfaces/quizoot.indexed';
+
+export const state = {
+    quizzes: null,
+};
+
+export type State = {
+    quizzes: Quiz[] | null;
+};
