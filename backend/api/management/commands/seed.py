f262747834e46a32831b719d81310203ed5058af
merchrist.kiki <alexismerchrist.kiki@gmail.com>
2023-02-22 00:24:09 +0100

    Seed data in the mongodb database

--- backend/api/management/commands/seed.py
+++ backend/api/management/commands/seed.py
@@ -1,7 +1,10 @@
 from django.core.management.base import BaseCommand
 from argparse import ArgumentParser
+from django.conf import settings
 
-from backend.api.client import client
+from backend.api.client import client, CollectionName
+import json
+
 
 # USAGE :
 #   python manage.py seed --mode=(refresh | clear)
@@ -9,7 +12,12 @@ from backend.api.client import client
 MODE_CLEAR = "clear"
 MODE_REFRESH = "refresh"
 
+from pathlib import PosixPath
 
+DATA_DIR: PosixPath = settings.BASE_DIR / "data"
+
+
+# TODO: add a logger ?
 class Command(BaseCommand):
     help = "seed database for testing and development."
 
@@ -19,10 +27,38 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        if options.get("mode") == MODE_CLEAR:
-            return self.clear_data()
+        mode = options.get("mode")
+        # Always clear data
+        self.clear_data()
+        if mode == MODE_REFRESH:
+            for filepath in DATA_DIR.glob("*.json"):
+                collection_name = filepath.name[:-5]
+                self.seed_data(collection_name)
 
     def clear_data(self):
-        self.stdout.write("Deleting all collections...")
+        self.stdout.write("[INFO] Deleting all collections...")
         for name in client._collections:
-            client.collection(name).drop()
+            result = client.collection(name).delete_many({})
+            self.stdout.write(
+                "[INFO] Deleted %s documents from %s" % (result.deleted_count, name)
+            )
+
+    def seed_data(self, collection_name: CollectionName):
+        self.stdout.write('[INFO] seeding data in "%s" collection' % collection_name)
+        data = self._read_json_data(collection_name)
+        collection = client.collection(collection_name)
+        try:
+            collection.insert_many(data)
+            self.stdout.write(
+                "[INFO] %s documents inserted" % collection.count_documents({})
+            )
+        except Exception as err:
+            self.stderr.write(
+                '[ERROR] Unable to seed data from "%s/%s.json" to collection %s\n'
+                % (DATA_DIR, collection_name, collection_name)
+            )
+            self.stderr.write(err)
+
+    def _read_json_data(self, filename: str):
+        with open(DATA_DIR / f"{filename}.json") as file:
+            return json.load(file)
