26cb50ff5cc3d6be33691008ada32fd907aea0c7
Merchrist <58826859+chrichri17@users.noreply.github.com>
2023-02-23 10:46:44 +0100

    Better typing + schemas regeneration (#74)

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -3,34 +3,68 @@ import * as tjs from 'typescript-json-schema';
 
 import path from 'path';
 
-const SCHEMAS_DIR = path.join(__dirname, "./schemas");
+const SCHEMAS_DIR = path.join(__dirname, './schemas');
 const SOURCE_FILE_PATH = path.join(__dirname, './quizoot.ts');
 
 // Arguments to be passed to schema generator
 const SETTINGS: tjs.PartialArgs = {
-    required: true,
+	required: true,
 };
 
-const SCHEMAS_TO_GENERATE = [
-    {
-        type: 'Quizoot.Question',
-        name: 'question'
-    },
-    {
-        type: 'Quizoot.Quiz',
-        name: 'quiz'
-    }
-];
+const program = tjs.getProgramFromFiles([path.resolve(SOURCE_FILE_PATH)]);
 
+async function main() {
+	const schema = generateSchema('QuestionKind');
+	saveSchema('question_kind', schema);
+	const questionKinds = ((schema?.enum as string[]) ?? []).map((s) =>
+		s.toLowerCase()
+	);
 
-SCHEMAS_TO_GENERATE.forEach((schema) => {
-    const program = tjs.getProgramFromFiles([path.resolve(SOURCE_FILE_PATH)]);
-    const definition = tjs.generateSchema(program, schema.type, SETTINGS);
-    
-    try {
-        const filepath = path.join(SCHEMAS_DIR, `./${schema.name}.json`)
-        fs.writeFileSync(filepath, JSON.stringify(definition, null, 2) + '\n');
-    } catch (err) {
-        console.error(err);
-    }
-});
+	// Generate appropriate schema for each kind of question
+	for (const kind of questionKinds) {
+		try {
+			const schema = generateSchema(snakeToPascal(kind));
+			saveSchema(kind, schema);
+		} catch (err) {
+			console.error('Could not generate schema', kind);
+		}
+	}
+
+	// Generate question schema (union of all questions)
+	saveSchema('question', generateSchema('Question'));
+
+	// Generate quiz schema
+	saveSchema('quiz', generateSchema('Quiz'));
+}
+
+function generateSchema(typeName: string) {
+	console.log(`[INFO] Generating "Quizoot.${typeName}" schema`);
+	return tjs.generateSchema(program, `Quizoot.${typeName}`, SETTINGS);
+}
+
+async function saveSchema(name: string, schema: tjs.Definition | null) {
+	return fs.writeFile(
+		path.join(SCHEMAS_DIR, `${name}.json`),
+		JSON.stringify(schema, null, 4),
+		(err) => {
+			if (err) {
+				console.error(`unable to save ${name} schema.`);
+			}
+		}
+	);
+}
+
+function snakeToPascal(s: string) {
+	return s.toLowerCase().split('_').map(capitalize).join('');
+}
+
+function capitalize(s: string) {
+	return s[0].toUpperCase() + s.slice(1);
+}
+
+void main()
+	.then(() => console.log('[INFO] All schemas generated !'))
+	.catch((err) => {
+		console.error('[ERROR]', err);
+		process.exit(1);
+	});
